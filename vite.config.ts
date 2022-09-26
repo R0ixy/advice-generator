85345871a1b054f2ac72a2bdefe6dd7930508d01
R0ixy <nikita6kom@gmail.com>
2022-09-26 13:12:07 +0300

    configure basic project structure

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path";
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+});
