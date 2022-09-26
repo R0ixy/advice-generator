5c4bb7a7566a3de2fdd74f43306842faa33dbee1
R0ixy <nikita6kom@gmail.com>
2022-09-26 19:48:44 +0300

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/advice-generator',
+  base: '/advice-generator/',
   server: {
     host: true,
     port: 3000,
