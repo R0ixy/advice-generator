544f959c2a37b47a5e7d2820f9d88ef5054476f3
R0ixy <nikita6kom@gmail.com>
2022-09-26 16:28:29 +0300

    add advice fetching

--- src/components/advice-card/advice-card.tsx
+++ src/components/advice-card/advice-card.tsx
@@ -1,19 +1,42 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import styles from './styles.module.scss';
 
+interface iAdvice {
+  id: number;
+  text: string;
+}
+
 const AdviceCard: FC = () => {
+  const [advice, setAdvice] = useState<iAdvice>({ id: 0, text: '' });
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => {
+        response.json()
+          .then(res =>
+            setAdvice({
+              id: res.slip.id,
+              text: res.slip.advice
+            })
+          );
+      });
+  }
+
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.cardWrapper}>
-        <span className={styles.adviceNumber}>Advice #117</span>
+        <span className={styles.adviceNumber}>Advice #{advice.id}</span>
         <p className={styles.adviceText}>
-          "Once you find a really good friend don't do anything that could mess up your
-          friendship."
+          "{advice.text}"
         </p>
         <div className={styles.divider}/>
       </div>
-      <div className={styles.button} />
+      <div className={styles.button} onClick={fetchAdvice}/>
     </div>
   );
 };
